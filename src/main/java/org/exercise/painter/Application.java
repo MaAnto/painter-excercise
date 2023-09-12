39b69fee4e6a9ca63aa38f256f4f4922542dda93
GabrieleSprecacenere <gabriele.sprecacenere@aesys.tech>
2023-09-12 14:09:48 +0200

    Programma finito

--- src/main/java/org/exercise/painter/Application.java
+++ src/main/java/org/exercise/painter/Application.java
@@ -1,8 +1,12 @@
 package org.exercise.painter;
 
+import java.util.Scanner;
 public class Application {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Scanner in=new Scanner(System.in);
+    String color= in.next();
+    Painter giotto= new Painter("Giotto", color);
+    System.out.println(giotto.getName() + " sta dipingendo di colore " + giotto.getColor());
   }
 }
---
+++ src/main/java/org/exercise/painter/Painter.java
@@ -0,0 +1,18 @@
+package org.exercise.painter;
+
+public class Painter {
+    String nome, colore;
+
+    public Painter(String name, String color){
+        nome = name;
+        colore = color;
+    }
+
+    public String getName(){
+        return nome;
+    }
+
+    public String getColor(){
+        return colore;
+    }
+}
