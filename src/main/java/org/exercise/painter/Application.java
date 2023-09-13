17767ae97e633894fbd0a9e2fda818e99f3fd540
GabrieleSprecacenere <gabriele.sprecacenere@aesys.tech>
2023-09-13 11:07:49 +0200

    .

--- src/main/java/org/exercise/painter/Application.java
+++ src/main/java/org/exercise/painter/Application.java
@@ -3,22 +3,29 @@ package org.exercise.painter;
 import java.util.Scanner;
 
 public class Application {
-
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     ColorList listaColori=new ColorList();
-
     System.out.println("Di che colore vuoi che dipinga?");
 
     String color = in.next();
-    for(int i=0;i<listaColori.getLenght();i++){
-      if(color.equalsIgnoreCase(listaColori.getNome(i))){
-        Painter pittore = new Painter("Giotto", color);
+    if(controlloColori(color, listaColori)){
+      Painter pittore = new Painter("Giotto", color);
 
-        System.out.println(pittore.getName() + " sta dipingendo di colore " + pittore.getColor());
-        return;
+      System.out.println(pittore.getName() + " sta dipingendo di colore " + pittore.getColor());
+    }else {
+      System.out.println("Questo colore non lo conosco"); //colore non riconosciuto
+    }
+
+    in.close();
+  }
+
+  public static boolean controlloColori(String input, ColorList listaColori){
+    for(int i=0;i<listaColori.getLenght();i++){
+      if(input.equalsIgnoreCase(listaColori.getNome(i))){ //controllare che il colore inserito sia un colore esistente
+        return true; //termina il programma
       }
     }
-    System.out.println("Questo colore non lo conosco");
+    return false;
   }
 }
